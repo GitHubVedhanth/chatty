238c80762cebe79ba6fd58126b5eff7ffbbffdb7
thisisplanetdev <thisislily01992@gmail.com>
2025-07-12 18:56:17 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,21 @@
-import express from "express"
-import { authrouter } from "./routes/auth.route.js";
-import dotenv from 'dotenv';
-import { connet_db } from "./lib/db.js";
-import cookieParser from "cookie-parser";
-import messagerouter from "./routes/message.route.js";
-import cors from 'cors'
-import { app ,server,io} from "./lib/socket.js";
+import express from "express";
 import path from "path";
-const __dirname = path.resolve()
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import { authrouter } from "./routes/auth.route.js";
+import messagerouter from "./routes/message.route.js";
+import { connet_db } from "./lib/db.js";
+
+const __dirname = path.resolve();
 dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.use(express.json())
-app.use(express.json({ limit: '50mb' })); 
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -24,17 +24,16 @@ app.use(cors({
   credentials: true
 }));
 
-app.use('/auth',authrouter)
-app.use('/message',messagerouter)
+app.use('/auth', authrouter);
+app.use('/message', messagerouter);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
-server.listen(port,()=>{
-    connet_db()
-})
+server.listen(port, () => {
+  connet_db();
+});
