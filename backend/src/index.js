749e59ec4023494c510cb949b000db07fa63a6dc
thisisplanetdev <thisislily01992@gmail.com>
2025-07-12 18:42:06 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://chatty-8afs.onrender.com"
+  ],
+  credentials: true
+}));
+
 app.use('/auth',authrouter)
 app.use('/message',messagerouter)
 
