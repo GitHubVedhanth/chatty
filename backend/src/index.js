32b4da300086a21ca12f734cfd8517c3f79d9263
thisisplanetdev <thisislily01992@gmail.com>
2025-07-12 19:13:21 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,11 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.originalUrl);
+  next();
+});
+
 app.use(cookieParser());
 app.use(cors({
   origin: [
