fedc177ff28ad35dd899be4659742b14475ef696
thisisplanetdev <thisislily01992@gmail.com>
2025-07-12 19:07:47 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,19 @@
+
+import express from 'express'; 
 import dotenv from 'dotenv';
 import path from 'path';
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import { app, server } from "./lib/socket.js";  // ✅ USE THIS APP
-import { authrouter } from "./routes/auth.route.js";
-import messagerouter from "./routes/message.route.js";
-import { connet_db } from "./lib/db.js";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import { app, server } from './lib/socket.js'; 
+import { authrouter } from './routes/auth.route.js';
+import messagerouter from './routes/message.route.js';
+import { connet_db } from './lib/db.js';
 
 const __dirname = path.resolve();
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -27,14 +30,13 @@ app.use('/auth', authrouter);
 app.use('/message', messagerouter);
 
 if (process.env.NODE_ENV === 'production') {
-  console.log("Serving static files...");
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
   });
 }
 
 server.listen(port, () => {
   connet_db();
-  console.log(`Server running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
